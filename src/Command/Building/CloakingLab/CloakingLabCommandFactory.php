bf36ae9a980fed990a36ab41c8117f3ef2d90cce
Abyssinia <abyssinianempire@gmail.com>
2015-02-08 13:53:52 -0600

    ship building commands

---
+++ src/Command/Building/CloakingLab/CloakingLabCommandFactory.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace LacunaPHP\APIClient\Command\Building\CloakingLab;
+
+use LacunaPHP\APIClient\Command\Building\BuildingCommandFactory;
+
+
+class CloakingLabCommandFactory extends BuildingCommandFactory {}
---
+++ src/Command/Building/PilotTraining/PilotTrainingCommandFactory.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace LacunaPHP\APIClient\Command\Building\PilotTraining;
+
+use LacunaPHP\APIClient\Command\Building\BuildingCommandFactory;
+
+
+class PilotTrainingCommandFactory extends BuildingCommandFactory {}
