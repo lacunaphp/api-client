2866f5f96a0f5e01eb47cf42b27a3f94fe7fdfcb
Abyssinia <abyssinianempire@gmail.com>
2015-02-08 13:21:21 -0600

    energy buildings

---
+++ src/Command/Building/Fission/FissionCommandFactory.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace LacunaPHP\APIClient\Command\Building\Fission;
+
+use LacunaPHP\APIClient\Command\Building\BuildingCommandFactory;
+
+
+class FissionCommandFactory extends BuildingCommandFactory {}
