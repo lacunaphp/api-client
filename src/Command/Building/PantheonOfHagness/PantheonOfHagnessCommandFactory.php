e8ef5a519c5a8a72645a0273842645230eb28372
Abyssinia <abyssinianempire@gmail.com>
2015-02-08 15:18:15 -0600

    pantheon of hagness

---
+++ src/Command/Building/PantheonOfHagness/PantheonOfHagnessCommandFactory.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace LacunaPHP\APIClient\Command\Building\PantheonOfHagness;
+
+use LacunaPHP\APIClient\Command\Building\BuildingCommandFactory;
+
+
+class PantheonOfHagnessCommandFactory extends BuildingCommandFactory {}
