1a6314bc0731e86f7fed87101147497b87892dfd
Abyssinia <abyssinianempire@gmail.com>
2015-02-08 13:54:34 -0600

    terra labs and platforms

---
+++ src/Command/Building/TerraformingLab/TerraformingLabCommandFactory.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace LacunaPHP\APIClient\Command\Building\TerraformingLab;
+
+use LacunaPHP\APIClient\Command\Building\BuildingCommandFactory;
+
+
+class TerraformingLabCommandFactory extends BuildingCommandFactory {}
---
+++ src/Command/Building/TerraformingPlatform/TerraformingPlatformCommandFactory.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace LacunaPHP\APIClient\Command\Building\TerraformingPlatform;
+
+use LacunaPHP\APIClient\Command\Building\BuildingCommandFactory;
+
+
+class TerraformingPlatformCommandFactory extends BuildingCommandFactory {}
