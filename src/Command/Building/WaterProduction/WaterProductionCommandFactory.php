ecdb2fe6dd9b54d94038872e2fa2e3c330c2b962
Abyssinia <abyssinianempire@gmail.com>
2015-02-08 13:23:06 -0600

    water building commands

---
+++ src/Command/Building/WaterProduction/WaterProductionCommandFactory.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace LacunaPHP\APIClient\Command\Building\WaterProduction;
+
+use LacunaPHP\APIClient\Command\Building\BuildingCommandFactory;
+
+
+class WaterProductionCommandFactory extends BuildingCommandFactory {}
