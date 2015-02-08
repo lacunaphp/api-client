82d38b2a59af042b35dfe86855936acc2529822f
Abyssinia <abyssinianempire@gmail.com>
2015-02-08 16:05:13 -0600

    warehouse ss module

---
+++ src/Command/Building/Warehouse/WarehouseCommandFactory.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace LacunaPHP\APIClient\Command\Building\Warehouse;
+
+use LacunaPHP\APIClient\Command\Building\BuildingCommandFactory;
+
+
+class WarehouseCommandFactory extends BuildingCommandFactory {}
