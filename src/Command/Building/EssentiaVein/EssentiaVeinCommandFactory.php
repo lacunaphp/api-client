03ac2cda2789feeae048b406cc77fafff7740343
Abyssinia <abyssinianempire@gmail.com>
2015-02-08 15:15:39 -0600

    evein

---
+++ src/Command/Building/EssentiaVein/EssentiaVeinCommandFactory.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace LacunaPHP\APIClient\Command\Building\EssentiaVein;
+
+use LacunaPHP\APIClient\Command\Building\BuildingCommandFactory;
+
+
+class EssentiaVeinCommandFactory extends BuildingCommandFactory {}
