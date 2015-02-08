56834b103159a8a663f2910cc10f088d9a6d9e41
Abyssinia <abyssinianempire@gmail.com>
2015-02-08 15:14:20 -0600

    kalavian ruins

---
+++ src/Command/Building/KalavianRuins/KalavianRuinsCommandFactory.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace LacunaPHP\APIClient\Command\Building\KalavianRuins;
+
+use LacunaPHP\APIClient\Command\Building\BuildingCommandFactory;
+
+
+class KalavianRuinsCommandFactory extends BuildingCommandFactory {}
