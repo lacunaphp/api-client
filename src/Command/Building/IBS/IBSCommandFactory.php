784d2869b700bf79336bd9ec509986d8c8e1c956
Abyssinia <abyssinianempire@gmail.com>
2015-02-08 16:04:46 -0600

    ibs ss module

---
+++ src/Command/Building/IBS/IBSCommandFactory.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace LacunaPHP\APIClient\Command\Building\IBS;
+
+use LacunaPHP\APIClient\Command\Building\BuildingCommandFactory;
+
+
+class IBSCommandFactory extends BuildingCommandFactory {}
